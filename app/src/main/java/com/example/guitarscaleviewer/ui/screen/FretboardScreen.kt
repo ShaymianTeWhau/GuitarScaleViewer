4a1d8760441b09ef251ae234e8fbbc4fdbe08661
ShaymianTeWhau <shaytewhau@gmail.com>
2025-09-27 12:30:54 +1200

    add fretboard screen composable

---
+++ app/src/main/java/com/example/guitarscaleviewer/ui/screen/FretboardScreen.kt
@@ -0,0 +1,9 @@
+package com.example.guitarscaleviewer.ui.screen
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun FretboardScreen() {
+    Text("Hello Fretboard")
+}
